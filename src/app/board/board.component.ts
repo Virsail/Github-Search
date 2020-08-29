0ac2a28007ec4ec4e07789cfb4df7a1b30fee1a8
virsail <virsaileric@gmail.com>
2020-08-29 22:39:15 +0300

    add teh user componenet

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,6 +3,10 @@ import { Observable} from 'rxjs';
 import { Repo} from '../repo';
 import { GithubService } from '../github.service';
 import {environment} from '../../environments/environment';
+import {ProfileRequestService} from '../profile-request.service';
+import { User } from '../user';
+import { SweetAlertService } from 'angular-sweetalert-service';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-board',
