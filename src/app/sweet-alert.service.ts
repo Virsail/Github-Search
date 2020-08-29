431104eede72f399d86274a03dff36e164fc2cab
virsail <virsaileric@gmail.com>
2020-08-29 17:16:17 +0300

    add the environments to my sweet alert service

--- src/app/sweet-alert.service.ts
+++ src/app/sweet-alert.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { user } from './user';
-
+import {environment} from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
