6b02867a74e8570547309de989183f9655073b73
virsail <virsaileric@gmail.com>
2020-08-29 19:33:58 +0300

    make the changes made saved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BoardComponent } from './board/board.component';
 
 @NgModule({
