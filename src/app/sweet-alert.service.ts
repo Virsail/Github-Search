ee42038315d3ac79beafb3d6d4326e1bb47b4fb6
virsail <virsaileric@gmail.com>
2020-08-29 17:20:52 +0300

    add the prompt ,error and alert warnings to my sweet alert2 service

--- src/app/sweet-alert.service.ts
+++ src/app/sweet-alert.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
-import { user } from './user';
+import { User } from './user';
 import {environment} from '../environments/environment';
-@Injectable({
-  providedIn: 'root'
-})
-export class SweetAlertService {
+@Injectable()
 
-  constructor() { }
+export declare class SweetAlertService {
+  constructor();
+  swal(): any;
+  confirm(options: any): any;
+  prompt(options: any): any;
+  alert(options: any): any;
+  question(options: any): any;
+  success(options: any): any;
+  warning(options: any): any;
+  error(options: any): any;
+  info(options: any): any;
 }
+
+
+
+
+
