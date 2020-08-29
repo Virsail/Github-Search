f5f3c93a0129e2fb5ebb356229c454e57699d32a
virsail <virsaileric@gmail.com>
2020-08-29 22:34:54 +0300

    add the host ,listener to my .ts file

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { Observable} from 'rxjs';
 import { Repo} from '../repo';
 
