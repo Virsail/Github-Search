79db08ab063ebe99ec0e91a787c582abf161ad91
virsail <virsaileric@gmail.com>
2020-08-29 19:49:38 +0300

    add the changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+
 
   ],
   providers: [],
