b6cbc53bcb4b459519a7131fade9b1420d29a3cf
virsail <virsaileric@gmail.com>
2020-08-29 17:51:08 +0300

    add the environments to my request service

--- src/app/profile-request.service.ts
+++ src/app/profile-request.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { User } from './user';
+import {environment} from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
